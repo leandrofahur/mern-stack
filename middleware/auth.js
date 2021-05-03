bbddb7bcb71346b743473c0915320deedd813903
Leandro <fahurleandro@gmail.com>
2021-05-03 14:30:24 -0700

    restructure

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,4 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-module.exports = function (req, res, next) {
-  const token = req.header("x-auth-token");
-  if (!token) {
-    return res.status(401).json({ msg: "No token, authorization denied!" });
-  }
-
-  // verify token
-  try {
-    const decoded = jwt.verify(token, config.get("jwtToken"));
-    req.user = decoded.user;
-    next();
-  } catch (err) {
-    res.status(401).json({ msg: "Token is not valid" });
-  }
-};
+module.exports = function (req, res, next) {};
--- routes/api/post.js
+++ routes/api/post.js
@@ -1,11 +1,4 @@
 const express = require("express");
 const router = express.Router();
 
-// @route   GET api/post
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => {
-  res.send("Post route");
-});
-
 module.exports = router;
