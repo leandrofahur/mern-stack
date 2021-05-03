1e994af2c97072b753220e4eb7449c1a9febba55
Leandro <fahurleandro@gmail.com>
2021-05-03 16:01:25 -0700

    user POST route with validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,34 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator/check");
+
 const router = express.Router();
+
+// @route   POST api/users
+// @desc    Register user
+// @access  Public
+
+router.post(
+  "/",
+  [
+    check("username", "Username is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    res.status(200).json({
+      data: req.body,
+    });
+  }
+);
 
 module.exports = router;
