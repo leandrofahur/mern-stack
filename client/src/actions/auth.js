e6e72a5afe083fdc70d855e5dd97b7352e3b65d6
Leandro <fahurleandro@gmail.com>
2021-05-04 16:27:14 -0700

    register

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,27 +1,45 @@
 import axios from "axios";
+import { response } from "express";
+import { setAlert } from "./alert";
 import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
 
 // Register User:
-export const register = ({ name, email, password }) => {
-  async (dispatch) => {
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-    const body = JSON.stringify({ name, email, password });
+export const register = ({ name, email, password }) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-    try {
-      const res = await axios.post("/api/users", body, config);
-      dispatch({
-        type: REGISTER_SUCCESS,
-        payload: res.data,
-      });
-    } catch (error) {
-      console.log(error.message);
-      dispatch({
-        type: REGISTER_FAIL,
+  const newUser = {
+    name,
+    email,
+    password,
+  };
+  const body = JSON.stringify(newUser);
+
+  try {
+    const res = await axios.post(
+      "http://localhost:5000/api/users",
+      body,
+      config
+    );
+
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: response.data,
+    });
+  } catch (error) {
+    const errors = error.response.data.errors;
+    console.log(errors);
+    if (errors) {
+      errors.forEach((error) => {
+        dispatch(setAlert(error.msg, "danger"));
       });
     }
-  };
+    console.log(error.message);
+    dispatch({
+      type: REGISTER_FAIL,
+    });
+  }
 };
