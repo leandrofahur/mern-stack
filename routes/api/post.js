6dc2c1f345776d190d10ecba4003cb2d290d3652
Leandro <fahurleandro@gmail.com>
2021-05-04 00:02:48 -0700

    remove post by add

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,4 +1,141 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+const auth = require("../../middleware/auth");
+
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route   GET api/posts
+// @desc    Fetch all posts
+// @access  Private
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+
+    res.status(200).json({
+      posts,
+    });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
+// @route   GET api/posts/:post_id
+// @desc    Fetch post by ID
+// @access  Private
+
+router.get("/:post_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({
+        error: "Post not found",
+      });
+    }
+
+    res.status(200).json({
+      post,
+    });
+  } catch (error) {
+    console.error(error.message);
+
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({
+        error: "Post not found",
+      });
+    }
+
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
+// @route   POST api/posts
+// @desc    Create or Update a post
+// @access  Public
+
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.status(200).json({
+        post,
+      });
+    } catch (error) {
+      console.error(error.message);
+      return res.status(500).json({
+        error: error.message,
+      });
+    }
+  }
+);
+
+// @route   DELETE api/posts/:post_id
+// @desc    Delete post by ID
+// @access  Private
+
+router.delete("/:post_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({
+        error: "Post not found",
+      });
+    }
+
+    // Check on the user:
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({
+        message: "User not authorized!",
+      });
+    }
+
+    await post.remove();
+
+    res.status(200).json({
+      message: "Post removed",
+    });
+  } catch (error) {
+    console.error(error.message);
+
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({
+        error: "Post not found",
+      });
+    }
+
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+});
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const Profile = require("../../models/Profile");
 const { check, validationResult } = require("express-validator/check");
 const User = require("../../models/User");
 const auth = require("../../middleware/auth");
+const request = require("request");
+const config = require("config");
 
 // @route   GET api/profile/me
 // @desc    Fetch current user
@@ -381,6 +383,39 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
 
     res.status(200).json({
       profile,
+    });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from github
+// @access  Public
+
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientID"
+      )}&client_secret=${config.get("githubClientSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        res.status(404).json({ message: "No github profile found!" });
+      }
+
+      res.json(JSON.parse(body));
     });
   } catch (error) {
     console.error(error.message);
