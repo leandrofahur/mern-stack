c9a3eed385c8f46a0a8e24f4a05067f76f750a43
Leandro <fahurleandro@gmail.com>
2021-05-04 14:11:19 -0700

    redux boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,29 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/Register";
 import Login from "./components/Login";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing}></Route>
-        <section className="container">
-          <Switch>
-            <Route exact path="/login" component={Login}></Route>
-            <Route path="/register" component={Register}></Route>
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing}></Route>
+          <section className="container">
+            <Switch>
+              <Route exact path="/login" component={Login}></Route>
+              <Route path="/register" component={Register}></Route>
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
